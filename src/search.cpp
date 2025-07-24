dc5a359304dc90bde902c78853e118e6b88411dc
87 <dev@87flowers.com>
2025-07-24 17:16:42 +0100

    Remove eval fudge
    Bench: 1385051

--- src/search.cpp
+++ src/search.cpp
@@ -207,7 +207,7 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
     }
 
     if (!PV_NODE && !is_in_check && depth >= tuned::nmp_depth && tt_adjusted_eval >= beta) {
-        int      R         = tuned::nmp_base_r + std::min(3, (tt_adjusted_eval - beta) / 300);        
+        int      R         = tuned::nmp_base_r + std::min(3, (tt_adjusted_eval - beta) / 300);
         Position pos_after = pos.null_move();
 
         m_repetition_info.push(pos_after.get_hash_key(), true);
@@ -255,7 +255,7 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
             }
         }
 
-        Value see_threshold = quiet ? -67 * depth : -64 * depth;        
+        Value see_threshold = quiet ? -67 * depth : -64 * depth;
         // SEE PVS Pruning
         if (depth <= 10 && !ROOT_NODE && !SEE::see(pos, m, see_threshold)) {
             continue;
@@ -455,9 +455,7 @@ Value Worker::evaluate(const Position& pos) {
         mobility -= 10 * std::popcount(x);
     }
 
-    Value fudge = static_cast<i32>(search_nodes & 7) - 3;
-
-    return material + mobility + fudge;
+    return material + mobility;
 }
 }
 }
